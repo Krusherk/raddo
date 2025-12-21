b407f0b8c57a2f01579bf898ec6e7374c0598022
Krusherk <89709403+Krusherk@users.noreply.github.com>
2025-12-21 15:18:23 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 3000,
+        open: true
+    },
+    define: {
+        global: 'globalThis',
+    },
+    resolve: {
+        alias: {
+            buffer: 'buffer/',
+        },
+    },
+    optimizeDeps: {
+        include: ['buffer'],
+    },
 })
